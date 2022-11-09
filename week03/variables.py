920326ee52e96584e75ebf14182bdaee05456994
Luke Mostiler <rangerrick11111111@gmail.com>
2022-11-09 16:13:59 -0500

    week three homework

---
+++ week03/variables.py
@@ -0,0 +1,13 @@
+from xml.dom import DomstringSizeErr
+from xmlrpc.server import DocCGIXMLRPCRequestHandler
+
+
+10 20
+x is 10 and y is 20
+10 multiplied by 20 is 200
+10 multipied by 20 is 200
+13.5 divided by 20 is 0.675
+10 plus 20 is 30
+pets: cat dog
+10x20=200
+10 plus 20 is 30
