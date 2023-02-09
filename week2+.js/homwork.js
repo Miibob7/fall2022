42fc93e94279f3c462f94e48b7f65be0f1550501
Luke Mostiler <rangerrick11111111@gmail.com>
2023-02-08 23:49:04 -0500

    welp, i guess idk how to spell compromise

---
+++ week2+.js/homwork.js
@@ -0,0 +1,10 @@
+x=10
+y=20
+console.log(x,y)
+console.log("x is", x, "and y is", y)
+console.log(x, "multiplied by",y, "is",x*y)
+x=13.5
+console.log(x, "divided by", y, "is", x/y)
+a1="cat"
+a2="dog"
+console.log("pets:", a1,a2)
