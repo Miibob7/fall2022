1746410ea6eeaa5377b8343263da755483f0aa63
Luke Mostiler <rangerrick11111111@gmail.com>
2023-02-10 10:17:05 -0500

    E

---
+++ week3+js/homework.js
@@ -0,0 +1,20 @@
+score = 0
+answer = input("whats nine plus ten?")
+if (answer == "21") {
+    (score = score + 1) 
+    console.log ("good job kiddo")}
+else{
+    console.log("its '19'.")}
+answer = input("what color is a carrot? ")
+if (answer == "green"){
+    console.log("good.")}
+    (score = score +1)
+else{
+    console.log("bruh moment.")}
+(answer = input("what is my mother's first name?"))
+(if answer == "mom"){
+    (score = score + 1)}
+    console.log("brother!")
+else{
+    console.log("you don't have my father's blessing.")}
+console.log("your score is", score)
